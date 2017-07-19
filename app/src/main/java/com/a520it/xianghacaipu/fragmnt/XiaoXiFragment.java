5c64b2ed6d860297f7693d6068f316a4fccf0a86
MSHAO1 <1040182715@qq.com>
2017-07-19 19:29:38 +0800

    测试

--- app/src/main/java/com/a520it/xianghacaipu/fragmnt/XiaoXiFragment.java
+++ app/src/main/java/com/a520it/xianghacaipu/fragmnt/XiaoXiFragment.java
@@ -15,6 +15,7 @@ public class XiaoXiFragment extends  BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_xiaoxi_layout, container, false);
 
+        //??
         return view;
     }
 }
